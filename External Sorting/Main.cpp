7f89c6368add3055959b0705ab7ec8aac13c7747
mitansh1398 <mitansh1398@iitg.ernet.in>
2019-04-05 00:14:29 +0530

    external commit 3

--- External Sorting/Main.cpp
+++ External Sorting/Main.cpp
@@ -2,17 +2,24 @@
 #include "MainMemory.h"
 #include "DiskFile.h"
 #include "ExtMergeSort.h"
-
+int DISK_PAGE_SIZE;
+int MEM_FRAME_SIZE; 
 int main()
 {
+	cout<<"Enter Buffer Size: "<<endl;
 	int x;
 	//reads size of main memory in terms of number of frames available
 	cin >> x;
+	cout<<"Enter page and frame size: "<<endl;
+	cin>>DISK_PAGE_SIZE;
+	MEM_FRAME_SIZE = DISK_PAGE_SIZE;
 	
 	//create main memory of x frames
 	MainMemory mm(x);
 
-
+	cout<<"Do you want Double buffering?"<<endl;
+	int db_flag;
+	cin>>db_flag;
 	//create a file by taking input from cin
 	DiskFile f;
 	f.readDiskFile();
@@ -21,7 +28,13 @@ int main()
 	ExtMergeSort e;
 	
 	//call 2 way externalmerge sort
-	e.multiWaySort(f,mm);
+
+	if(db_flag){
+		e.DBmultiWaySort(f,mm);
+	} else {
+		e.multiWaySort(f,mm);
+	}
+	
 
 	//output file by using cout
 	f.writeDiskFile(); 
--- External Sorting/common.h
+++ External Sorting/common.h
@@ -1,10 +1,10 @@
-#define DISK_PAGE_SIZE 3
-#define MEM_FRAME_SIZE DISK_PAGE_SIZE
 #include <iostream>
 #include <cmath>
 #include <vector>
 #include <algorithm>
 #include <queue>
+extern int DISK_PAGE_SIZE;
+extern int MEM_FRAME_SIZE;
 // #include <bits/stdc++.h>
 
 using namespace std;
